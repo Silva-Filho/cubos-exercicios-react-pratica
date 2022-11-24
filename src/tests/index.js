03be3a871495f66a34073e6a4ce02231453d5c16
Silva Filho <tarusilva@gmail.com>
2022-11-24 19:25:23 -0300

    feat: cria arquivo de imports para uso em testes
    Cria arquivo de centralização dos imports para uso em testes das
    bibliotecas:
    
    - @testing-library/user-event;
    
    - @testing-library/react-hooks; e
    
    - @testing-library/react.

---
+++ src/tests/index.js
@@ -0,0 +1,12 @@
+import { cleanup, render, screen, waitFor } from "@testing-library/react";
+import { renderHook } from "@testing-library/react-hooks"
+import userEvent from "@testing-library/user-event";
+
+export { 
+    cleanup,
+    render, 
+    renderHook, 
+    screen, 
+    userEvent, 
+    waitFor, 
+};
