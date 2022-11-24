2bd751b7f219683afb16aaced89b69c4cfe968ec
Silva Filho <tarusilva@gmail.com>
2022-11-23 23:03:20 -0300

    refactor: atualiza importação do arquivo CSS
    Atualiza importação do arquivo CSS de index.css para global.css.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import { Home } from './pages/Home';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { Home } from "./pages/Home";
+
+import "./global.css";
 
 ReactDOM.render(
     <React.StrictMode>
         <Home />
     </React.StrictMode>,
-    document.getElementById('root')
+    document.getElementById( "root" )
 );
