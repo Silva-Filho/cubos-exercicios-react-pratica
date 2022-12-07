37dc633d4a7c59abe7dfed03bf1b381d9f7e4dbd
Silva Filho <tarusilva@gmail.com>
2022-12-07 20:52:44 -0300

    style: aplica CSS Modules
    - Aplica estilos conforme CSS Modules;
    
    - Aplica regras de aspas duplas e espaçamento.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,52 +1,49 @@
-import { useState } from 'react';
+import React, { useState } from "react";
 
-import { musics } from '../../data/musics';
+import { musics } from "../../data/musics";
 
-import { Footer } from '../../components/Footer';
-import { Header } from '../../components/Header';
-import { Main } from '../../components/Main';
+import { Footer } from "../../components/Footer";
+import { Header } from "../../components/Header";
+import { Main } from "../../components/Main";
 
-import './styles.css';
+import styles from "./Home.module.css";
 
 export function Home() {
     // @ts-ignore
     // eslint-disable-next-line no-unused-vars
-    const [musicsInfos, setMusicsInfos] = useState([...musics]);
-    const [music, setMusic] = useState(null);
-    const [isPlaying, setIsPlaying] = useState(false);
+    const [ musicsInfos, setMusicsInfos ] = useState( [ ...musics ] );
+    const [ music, setMusic ] = useState( null );
+    const [ isPlaying, setIsPlaying ] = useState( false );
 
     /* const inicial = 0;
     const final = musicsInfos.length; */
-    //const musicPlayer = {
-        //music,
-        //setMusic: setMusic(),
-        //isPlaying,
-        /* Deve-se criar uma fç se quiser usar. */
-        //setIsPlaying: setIsPlaying(false)
-    //};
+    /* const musicPlayer = {
+        music,
+        setMusic: setMusic( this.music ),
+        isPlaying,
+        // Deve-se criar uma fç se quiser usar.
+        // setIsPlaying: setIsPlaying( false )
+        setIsPlaying: setIsPlaying( !this.isPlaying )
+    }; */
 
-    function handlePlayMusic(musicPlay) {
-        setMusic(musicPlay);
-        setIsPlaying(false);
-        /* console.log(musicPlay);
-        console.log();
-        console.log(music); */
+    function handlePlayMusic( musicPlay ) {
+        setMusic( musicPlay );
+        setIsPlaying( false );
     }
 
     return (
-        <div className="container">
+        <div className={styles.container}>
             <Header />
 
-            <Main musicsInfos={musicsInfos} handlePlayMusic={handlePlayMusic} />
+            <Main musicsInfos={ musicsInfos } handlePlayMusic={ handlePlayMusic } />
 
-            {/* <Footer music={music} inicial={inicial} final={final} /> */}
-            <Footer 
-                music={music} 
-                musicsInfos={musicsInfos} 
-                handlePlayMusic={handlePlayMusic} 
+            <Footer
+                music={ music }
+                musicsInfos={ musicsInfos }
+                handlePlayMusic={ handlePlayMusic }
                 /* musicPlayer={musicPlayer} */
-                isPlaying={isPlaying} 
-                setIsPlaying={setIsPlaying}
+                isPlaying={ isPlaying }
+                setIsPlaying={ setIsPlaying }
             />
         </div>
     );
