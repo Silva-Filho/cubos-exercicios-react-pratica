57acc114fa0bbab1f93fc2822600a9eacb3014ac
Silva Filho <tarusilva@gmail.com>
2022-12-09 22:34:13 -0300

    feat: adiciona act aos imports usados nos testes

--- src/tests/index.js
+++ src/tests/index.js
@@ -1,8 +1,9 @@
-import { cleanup, render, screen, waitFor } from "@testing-library/react";
+import { act, cleanup, render, screen, waitFor } from "@testing-library/react";
 import { renderHook } from "@testing-library/react-hooks"
 import userEvent from "@testing-library/user-event";
 
 export { 
+    act, 
     cleanup,
     render, 
     renderHook, 
